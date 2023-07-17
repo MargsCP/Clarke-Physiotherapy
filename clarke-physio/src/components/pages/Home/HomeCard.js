5ca554067a8fee1c6b1bf330cf9c3023c38718ef
Giovanni Smith <giovanni.smith@duke.edu>
2023-07-17 10:43:28 +0200

    more styling

--- clarke-physio/src/components/pages/Home/HomeCard.js
+++ clarke-physio/src/components/pages/Home/HomeCard.js
@@ -15,10 +15,9 @@ export default function HomeCard (props){
     }}
     className = {`theCard ${flipped ? ('isFlipped') : ('')}`}>
          
-        <div className="theFront" style={{backgroundImage: `url( ${props.service.image})`, backgroundSize: "cover", backgroundPosition: "center", opacity: 0.80}}>
-          <div className= "serviceTitle" style={{textAlign: "center", objectFit: "cover" }}>
-            <h3 style={{display: "grid", justifyContent: "center",alignItems: "center", fontWeight: "bold", marginTop: 20, opacity: 1.1}}>{props.service.name}</h3>
-          </div>
+        <div className="theFront" style={{backgroundImage: `url( ${props.service.image})`, backgroundSize: "cover", backgroundPosition: "center", opacity: 0.75,}}>
+            <h3 style={{ textAlign: "center", justifyContent: "center", alignItems: "center", fontWeight: "bold", fontFamily: 'Prompt', marginTop: 20}}>{props.service.name}</h3>
+          
         </div>
         <div className="theBack" style={{objectFit: "cover"}} > 
           <h4 style={{textAlign: "center", fontFamily: "Changa One"}}> {props.service.name}</h4>
